8eecdfa11d2a585d2588301d5d74eb0e5f327dfd
FelipeIgansi <lipeigansi@gmail.com>
2024-09-23 15:10:04 -0300

    Add showSearchTextField to handle the SearchTextField, and made the navigationRail implementation on main class.

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,15 +1,12 @@
 import androidx.compose.desktop.ui.tooling.preview.Preview
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
-import androidx.compose.material.Icon
-import androidx.compose.material.IconButton
+import androidx.compose.material.ExperimentalMaterialApi
 import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
 import androidx.compose.material.Text
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Menu
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -18,12 +15,10 @@ import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.*
-import homeStore.bannerRed
-import homeStore.searchBar
-import homeStore.topMenuButton
-import homeStore.verticalListProgramsItems
+import homeStore.*
 import theme.blackBackground
 
+@OptIn(ExperimentalMaterialApi::class)
 @Composable
 @Preview
 fun app() {
@@ -35,7 +30,8 @@ fun app() {
 
   var textSearch by remember { mutableStateOf("") }
 
-  var showLateralMenu by remember { mutableStateOf(false) }
+  var showCategoriesOptions by remember { mutableStateOf(true) }
+  var showSearchTextField by remember { mutableStateOf(false) }
 
   MaterialTheme {
     Box(Modifier.fillMaxSize().background(blackBackground)) {
@@ -63,6 +59,23 @@ fun app() {
               }
             } else searchBar(textSearch) { newText -> textSearch = newText }
           }
+    Surface(color = blackBackground) {
+      Box(Modifier.fillMaxSize()) {
+        Row {
+          navigationRail(
+            showCategoriesOptions = showCategoriesOptions,
+            showSearchTextField = showSearchTextField,
+            onShowCategoriesOptionsChanged = { newValue -> showCategoriesOptions = newValue },
+            onShowSearchTextFieldChanged = { newValue -> showSearchTextField = newValue }
+          )
+
+              if (showCategoriesOptions) {
+                    }
+                  }
+                }
+              }
+              if (showSearchTextField) searchBar(textSearch) { newText -> textSearch = newText }
+            }
 
           bannerRed()
 
